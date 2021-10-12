73c030ebe5046aa16aa1dc178bab23287e28fab2
youngermax <60242131+YoungerMax@users.noreply.github.com>
2021-10-12 18:54:30 -0400

    Add a check to ensure that the client can connect to Redis

--- src/main.go
+++ src/main.go
@@ -3,28 +3,43 @@ package main
 import (
 	"net/http"
 	"log"
+	"context"
 
 	"github.com/gorilla/mux"
-	"context"
     "github.com/go-redis/redis/v8"
 )
 
 var ctx = context.Background()
 var rdb *redis.Client
 
+// configuration
+var address  = "localhost:6379"
+var password = ""
+var database = 0
+
 func main() {
 	rdb = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-		Password: "",
-		DB: 0,
+		Addr: address,
+		Password: password,
+		DB: database,
 	})
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", docs).Methods("GET")
-	router.HandleFunc("/create", createLink).Methods("POST")
-	router.HandleFunc("/delete", deleteLink).Methods("DELETE")
-	router.HandleFunc("/clicks/{id}", getClicks).Methods("GET")
-	router.HandleFunc("/{id}", getLink).Methods("GET")
+	// ping redis
+	// make sure the client is connected to the server
+	ping := rdb.Ping(ctx)
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	if ping.Err() != nil {
+		_, err := ping.Result()
+		log.Fatalf("Could not connect to Redis!\n\nAddress: %s\nPassword: %s\nDatabase: %v\nError: %s", address, password, database, err.Error())
+	} else {
+		router := mux.NewRouter()
+
+		router.HandleFunc("/", docs).Methods("GET")
+		router.HandleFunc("/create", createLink).Methods("POST")
+		router.HandleFunc("/delete", deleteLink).Methods("DELETE")
+		router.HandleFunc("/clicks/{id}", getClicks).Methods("GET")
+		router.HandleFunc("/{id}", getLink).Methods("GET")
+
+		log.Fatal(http.ListenAndServe(":8000", router))
+	}
 }
